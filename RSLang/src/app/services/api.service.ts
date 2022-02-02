7989e9127b6b0b0d601db1e1ae691d6e31ab0c17
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2022-02-02 13:53:23 +0500

    feat: add methods ia api

--- RSLang/src/app/services/api.service.ts
+++ RSLang/src/app/services/api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { backendURL } from '../constants/backendURL';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  get(options: string) {
+    return this.http.get(backendURL + '/' + options);
+  }
+
+  post(options: string, obj: Object) {
+    return this.http.post(backendURL + '/' + options, obj);
+  }
+
+  delete(options: string) {
+    return this.http.delete<void>(backendURL + '/' + options);
+  }
+
+  put(options: string, obj: Object) {
+    return this.http.put(backendURL + '/' + options, obj);
+  }
 }
