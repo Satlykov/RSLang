ce56fc312c8fd7388240a47871e08103c606b21c
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2022-02-07 17:05:59 +0500

    fix: checkLogin and add new test build

--- RSLang/src/app/pages/sprint-game-page/sprint-game-page.component.ts
+++ RSLang/src/app/pages/sprint-game-page/sprint-game-page.component.ts
@@ -1,21 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
-
 @Component({
   selector: 'app-sprint-game-page',
   templateUrl: './sprint-game-page.component.html',
   styleUrls: ['./sprint-game-page.component.scss'],
 })
 export class SprintGamePageComponent implements OnInit {
-  constructor(private api: ApiService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.getWords();
-  }
-
-  getWords() {
-    this.api.get('words?group=5&page=1').subscribe((words) => {
-      console.log(words);
-    });
-  }
+  ngOnInit(): void {}
 }
