35baaa56ca449ef768b61a97e3642292bb8cc51b
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2022-02-07 12:28:35 +0500

    feat: add functions authorization

--- RSLang/src/app/components/core/main-content/main-content.component.ts
+++ RSLang/src/app/components/core/main-content/main-content.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { LayoutService } from 'src/app/services/layout.service';
+import { Subscription } from 'rxjs';
+import { AuthorizationService } from 'src/app/services/authorization.service';
 
 @Component({
   selector: 'app-main-content',
@@ -9,11 +11,27 @@ import { LayoutService } from 'src/app/services/layout.service';
 })
 export class MainContentComponent implements OnInit {
   @ViewChild('sidenav') public sidenav!: MatSidenav;
-  constructor(private layoutService: LayoutService) {}
+  authenticated = false;
+  private subs: Subscription = new Subscription();
+
+  constructor(
+    private layoutService: LayoutService,
+    private authorizationService: AuthorizationService
+  ) {}
 
   ngOnInit(): void {
     this.layoutService.sideNavToggleSubject.subscribe(() => {
       if (this.sidenav) this.sidenav.toggle();
     });
+    this.subs = this.authorizationService.authenticatedStatus$.subscribe(
+      (status: boolean) => {
+        this.authenticated = status;
+      }
+    );
+    this.authorizationService.checkLogin();
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
