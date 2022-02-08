e6877eca5425b3cbb825c5a03e6458cba691b8d3
SlikeX <thelastslike@gmail.com>
2022-02-08 13:41:09 +0500

    feat: add service

--- RSLang/src/app/pages/audio-call-game-page/audio-call-game-page.component.ts
+++ RSLang/src/app/pages/audio-call-game-page/audio-call-game-page.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { Word } from 'src/app/models/interface';
+import { AudioCallGameService } from 'src/app/services/audio-call-game.service';
 
 @Component({
   selector: 'app-audio-call-game-page',
@@ -10,10 +12,30 @@ export class AudioCallGamePageComponent implements OnInit {
 
   public isOpened:boolean = false
   public id: number = 0
+  // public temp: string = ''
+  // public temp2: string = ''
 
-  constructor() { }
+  constructor(
+  private aduioCallGameService: AudioCallGameService,
+  private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
+    // this.http.get<Word[]>('https://rslangsteam.herokuapp.com/words?group=5&page=1')
+    //   .subscribe(response =>{
+        // const words = JSON.parse(JSON.stringify(response)) ;
+        // console.log(words[0])
+        // this.temp = words[0].audio
+      //   console.log(response)
+      //   const words = response
+      //   const temp = words[0]
+      //   console.log(temp)
+      // })
+    this.aduioCallGameService.getQuestions()
+    .subscribe(response =>{
+      console.log(response)
+    })
+    console.log('hi')
   }
 
   public showQuestions(id: number):void {
@@ -21,4 +43,7 @@ export class AudioCallGamePageComponent implements OnInit {
     console.log(id)
   }
 
+  public checkResponse():void{
+
+  }
 }
--- RSLang/src/app/services/audio-call-game.service.ts
+++ RSLang/src/app/services/audio-call-game.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { Word } from '../models/interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AudioCallGameService {
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService
+  ) {}
+
+  getQuestions() {
+    return this.apiService.get('words?group=5&page=1')
+  }
 }
