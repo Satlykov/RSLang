7f38121abba721d646bbfa44946fb629a1bdcc90
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2022-02-08 20:37:29 +0500

    feat: UI for sprint game

--- RSLang/src/app/pages/sprint-game-page/sprint-game-page.component.ts
+++ RSLang/src/app/pages/sprint-game-page/sprint-game-page.component.ts
@@ -1,16 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
+import { SprintGameService } from 'src/app/services/sprint-game.service';
 @Component({
   selector: 'app-sprint-game-page',
   templateUrl: './sprint-game-page.component.html',
   styleUrls: ['./sprint-game-page.component.scss'],
 })
 export class SprintGamePageComponent implements OnInit {
-  constructor(private api: ApiService) {}
+  sprintStatus = false;
+  valueSpinner = 0;
+  startSecond = 5;
+  startSecondStatus = false;
+  gameSecond = 60;
 
-  ngOnInit(): void {
-    this.api.get('words').subscribe((req) => {
-      console.log(req);
-    });
+  constructor(private sprintGameService: SprintGameService) {}
+
+  ngOnInit(): void {}
+
+  startSprint() {
+    this.startSeconds();
+    setTimeout(() => {
+      this.startSecondStatus = false;
+      this.sprintStatus = true;
+      const gameSecondInterval = setInterval(() => {
+        this.gameSecond -= 1;
+        if (this.gameSecond === 0) {
+          clearInterval(gameSecondInterval);
+        }
+      }, 1000);
+    }, 5500);
+  }
+
+  stopSprint() {}
+
+  startSeconds() {
+    this.startSecondStatus = true;
+    let myInterval = setInterval(() => {
+      this.startSecond -= 1;
+      this.valueSpinner = 100 - 20 * this.startSecond;
+      if (this.startSecond === 0) {
+        clearInterval(myInterval);
+      }
+    }, 1000);
   }
 }
