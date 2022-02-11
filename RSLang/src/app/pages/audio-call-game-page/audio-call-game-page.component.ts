3c684ab58b2d2cf056ee3c8485d3676a55fb4b58
SlikeX <thelastslike@gmail.com>
2022-02-11 11:11:42 +0500

    feat:add random answer options func

--- RSLang/src/app/pages/audio-call-game-page/audio-call-game-page.component.ts
+++ RSLang/src/app/pages/audio-call-game-page/audio-call-game-page.component.ts
@@ -1,47 +1,95 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject, take, takeUntil } from 'rxjs';
 import { Word } from 'src/app/models/interface';
 import { AudioCallGameService } from 'src/app/services/audio-call-game.service';
+
+/*Вопросики:
+Как все это перенести в сервис, что должно быть в компоненте, что в сервисе?
+Можно ли юзать один и тот же сабжект в разных подписках?
+*/
 
 @Component({
   selector: 'app-audio-call-game-page',
   templateUrl: './audio-call-game-page.component.html',
   styleUrls: ['./audio-call-game-page.component.scss']
 })
-export class AudioCallGamePageComponent implements OnInit {
+export class AudioCallGamePageComponent implements OnInit, OnDestroy {
 
-  public isOpened:boolean = false;
-  public id: number = 0;
+  public isOpened = false;
+  public id = 0;
   public currentWordsPack: Word[] = [];
-  private forRandomAnsers: String[] = [];
+  private randomWords: String[] = [];
+  private randomWordsSubject = new Subject<void>();
+
+  private unsubscribe$ = new Subject<void>();
+
+  get temp$():Observable<void>{
+    return this.randomWordsSubject.asObservable()
+  }
 
   constructor(
   private audioCallGameService: AudioCallGameService,
   ) {}
 
   ngOnInit(): void {
-    this.getRandomAnswers();
+    this.getRandomWords();
+    this.randomWordsSubject
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe(()=>{
+      this.getRandomOptions()
+      })
+  }
+
+  ngOnDestroy():void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  private getRandomWords():void {
+    for(let id = 0; id<=5; id++){
+      this.audioCallGameService.getQuestions(id)
+        .pipe(
+          take(1)
+        )
+        .subscribe(response =>{
+          const words = response as Word[];
+          words.forEach(element => this.randomWords.push(element.word))
+          if(this.randomWords.length === 120){
+            this.randomWordsSubject.next();
+          }
+        })
+    }
+  }
+
+  private getRandomOptions():String[] {
+    const randomOptions: String[] = [];
+    while(randomOptions.length < 4){
+      const randomNum = Math.floor(Math.random() * (this.randomWords.length + 1))
+      randomOptions.push(this.randomWords[randomNum])
+    }
+    const randomNum = Math.floor(Math.random() * (this.randomWords.length + 1))
+    console.log(randomOptions)
+    return randomOptions
   }
 
   public showQuestions(id: number):void {
-    this.currentWordsPack = []
-    this.isOpened = !this.isOpened
-    this.audioCallGameService.getQuestions(id)
-      .subscribe(response =>{
-        const data = response as Word[];
-        data.forEach(element => this.currentWordsPack.push(element))
-      })
-    console.log(this.currentWordsPack)
+  this.currentWordsPack = []
+  this.isOpened = !this.isOpened
+  this.audioCallGameService.getQuestions(id)
+    .pipe(
+      take(1)
+    )
+    .subscribe(response =>{
+      const data = response as Word[];
+      data.forEach(element => this.currentWordsPack.push(element));
+      // if(this.currentWordsPack.length === 20){
+      //   this.randomWordsSubject.next();
+      // }
+      console.log(this.currentWordsPack)
+    })
   }
 
-  private getRandomAnswers():void {
-    for(let id = 0; id<=5; id++){
-      this.audioCallGameService.getQuestions(id)
-        .subscribe(response =>{
-          const words = response as Word[];
-          words.forEach(element => this.forRandomAnsers.push(element.word))
-        })
-    }
-    this.forRandomAnsers = [...new Set(this.forRandomAnsers)]
-  }
 }
--- RSLang/src/app/services/audio-call-game.service.ts
+++ RSLang/src/app/services/audio-call-game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, take } from 'rxjs';
 import { ApiService } from './api.service';
 import { Word } from '../models/interface';
 
@@ -10,19 +10,46 @@ export class AudioCallGameService {
 
   private maxPage = 29;
   private minPage = 0;
+  public randomWords: String[] = [];
+  public randomWordsSubject = new Subject<void>();
 
   constructor(
     private apiService: ApiService
   ) {}
 
-  getQuestions(group: number):Observable<Object> {
+
+
+  public getQuestions(group: number):Observable<Object> {
     const path = this.getPath(group);
     return this.apiService.get(path)
   }
 
-  getPath(group: number):string{
+  private getPath(group: number):string{
   const page = Math.floor(Math.random() * (this.maxPage - this.minPage + 1)) + this.minPage
   return `words?group=${group}&page=${page}`
   }
+
+
+
+  // public getRandomWords(): String[]{
+  //   for(let id = 0; id<=5; id++){
+  //     this.getQuestions(id)
+  //       .pipe(
+  //         take(1)
+  //       )
+  //       .subscribe(response =>{
+  //         const words = response as Word[];
+  //         words.forEach(element => this.randomWords.push(element.word))
+  //         if(this.randomWords.length === 120){
+  //           this.randomWordsSubject.next();
+  //         }
+  //       })
+  //   }
+  //   return this.randomWords
+  // }
+
+  // public getRandomOptions():void {
+  //   console.log(this.getRandomWords())
+  // }
 
 }
