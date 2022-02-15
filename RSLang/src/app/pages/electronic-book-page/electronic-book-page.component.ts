dd3113765ef0f292b6a5eb2a13113a61ca88680d
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2022-02-15 13:09:18 +0500

    feat: add btn for hard words

--- RSLang/src/app/pages/electronic-book-page/electronic-book-page.component.ts
+++ RSLang/src/app/pages/electronic-book-page/electronic-book-page.component.ts
@@ -8,6 +8,7 @@ import { ElectronicBookService } from 'src/app/services/electronic-book.service'
   styleUrls: ['./electronic-book-page.component.scss'],
 })
 export class ElectronicBookPageComponent implements OnInit {
+  authenticated = false;
   selected: string = 'group=0';
   levels = [
     { value: 'group=0', viewValue: 'A1 Elementary' },
@@ -16,11 +17,10 @@ export class ElectronicBookPageComponent implements OnInit {
     { value: 'group=3', viewValue: 'B2 Upper-Intermediate' },
     { value: 'group=4', viewValue: 'C1 Advanced' },
     { value: 'group=5', viewValue: 'C2 Proficiency' },
-    { value: 'group=6', viewValue: '"Сложные слова"' },
   ];
 
   backColors = [
-    'linear-gradient(90deg, rgba(34,193,195,1) 0%, rgba(175,253,45,1) 100%)',
+    'linear-gradient(90deg, rgba(36,29,253,1) 0%, rgba(39,255,0,1) 100%)',
     'linear-gradient(90deg, rgba(34,102,195,1) 0%, rgba(253,247,45,1) 100%)',
     'linear-gradient(90deg, rgba(253,85,29,1) 0%, rgba(252,176,69,1) 100%)',
     'linear-gradient(90deg, rgba(175,29,253,1) 0%, rgba(69,252,225,1) 100%)',
