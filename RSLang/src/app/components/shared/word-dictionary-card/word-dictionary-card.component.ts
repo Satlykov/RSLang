c7d99472ebe33913ecc3474b5b43989b38eb07bd
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2022-02-19 12:41:52 +0500

    fix: dicionary-card

--- RSLang/src/app/components/shared/word-dictionary-card/word-dictionary-card.component.ts
+++ RSLang/src/app/components/shared/word-dictionary-card/word-dictionary-card.component.ts
@@ -11,16 +11,17 @@ import { WordListComponent } from 'src/app/pages/word-list/word-list.component';
 })
 export class WordDictionaryCardComponent implements OnInit {
   @Input() card!: Word;
-  @Input() index!: number;
-  @Input() cards!: Word[];
 
   closed = false;
+  hard = false;
   constructor(
     private userWordService: UserWordService,
     private wordListComponent: WordListComponent
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.hard = this.card.userWord?.difficulty === 'hard';
+  }
 
   playWord(src: string) {
     let audio = new Audio();
@@ -50,10 +51,9 @@ export class WordDictionaryCardComponent implements OnInit {
     };
   }
 
-  deletHard() {
+  deleteHard() {
     this.userWordService.deleteUserWord(this.card._id).subscribe(() => {
       this.wordListComponent.getCards();
-      /* this.cards.splice(this.index, 1) */
     });
   }
 }
---
+++ RSLang/src/app/services/statistics.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Statistic } from '../models/interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StatisticsService {
+  allLearnedWords: number = 0;
+  today: string = new Date().toJSON().slice(0, 10);
+  allStatistics: Statistic = {
+    learnedWords: this.allLearnedWords,
+    optional: {
+      days: [
+        {
+          studyDay: '1',
+          day: '19.02.2022',
+          sprint: {
+
+          },
+          audio: {
+
+          },
+          book: {
+            
+          },
+        }
+      ],
+    },
+  };
+
+  constructor() {}
+}
